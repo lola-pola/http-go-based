c44d20f71c86edebf9cdac53d22b4bee0adcfaae
Elhay Efrat <elhayefrat@elhays.guest.corp.microsoft.com>
2022-12-29 17:22:51 +0200

    addding
    Signed-off-by: Elhay Efrat <elhayefrat@elhays.guest.corp.microsoft.com>

--- lola/lola.go
+++ lola/lola.go
@@ -4,14 +4,23 @@ import (
 	"fmt"
 	"net/http"
 	"time"
-
+	"math/rand"
 	"github.com/go-redis/redis"
 )
+
+
+func fibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return fibonacci(n-1) + fibonacci(n-2)
+}
 
 func main() {
 	// Connect to Redis
 	client := redis.NewClient(&redis.Options{
 		Addr:     "static-redis:6379",
+		// Addr:     "127.0.0.1:26379",
 		Password: "",
 		DB:       0,
 	})
@@ -22,9 +31,20 @@ func main() {
 
 		// Write the response
 		fmt.Fprint(w, "Hello, World!")
+		
 
+		
+		
+		
+
+		// rand.Seed(time.Now().UnixNano())
+		b := rand.Intn(100) + 1
+		fmt.Println(b)
+		fmt.Println(fibonacci(b))
 		// Store the response in Redis
-		err := client.Set("response", "Hello, World!", 0).Err()
+		latencys := time.Since(start)
+		lolas := latencys.String()
+		err := client.Set(lolas ,lolas , 0).Err()
 		if err != nil {
 			fmt.Println(err)
 			return
@@ -35,5 +55,5 @@ func main() {
 		fmt.Printf("Latency time: %s\n", latency)
 	})
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8085", nil)
 }
