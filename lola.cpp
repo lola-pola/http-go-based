520b39c1e46594cccd989322216a727d592e7e58
Elhay Efrat <elhayefrat@elhays.local>
2023-01-03 09:10:36 +0200

    asdda
    Signed-off-by: Elhay Efrat <elhayefrat@elhays.local>

---
+++ lola.cpp
@@ -0,0 +1,18 @@
+
+
+
+// write your code here cpp
+
+#include <iostream>
+
+
+// with this function you can read a line from the input
+int fib(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
